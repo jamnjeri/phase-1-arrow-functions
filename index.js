4fe0ebfa76934518b34e155da8810576e2d9c200
Jamila <jamnjeri@gmail.com>
2022-11-24 14:10:43 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,12 @@
+let num1;
+let num2;
+//Function that divides 2000 by 100
+function divide(){
+    return 2000 / 100
+}
+
+//Square Arrow function that takes one parameter and multiplies it times itself
+const square = num1 => num1*num1
+
+//Add Arrow function
+const add = (num1,num2) => num1+num2
